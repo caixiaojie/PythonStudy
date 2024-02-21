52417e2990f0c6299d97aea1a7cad43df9c1b090
蔡小杰 <m18408249795@163.com>
2024-02-21 18:10:41 +0800

    Python学习第二天 关键字，保留字，字符串

---
+++ chatp3/示例3-8字符串类型的使用.py
@@ -0,0 +1,19 @@
+city='天津'
+address='天津市宝坻区想家大姐'
+print(city)
+print(address)
+
+#多行字符串
+info=''' 地址：天津市宝坻区想家大姐'
+        收件人：dsd
+        手机号
+'''
+
+info2="""
+地址：天津市宝坻区想家大姐'
+        收件人：dsd
+        手机号
+"""
+
+print(info)
+print(info2)
